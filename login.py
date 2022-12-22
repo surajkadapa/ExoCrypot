58b27ac479b633ccc0c3d824b34af00fe1cd605e
surajkadapa <surajkadapa103@gmail.com>
2022-12-22 22:12:03 +0530

    Updated login page to check credentials from a text file(more changes in the future)

--- login.py
+++ login.py
@@ -2,11 +2,23 @@
 from tkinter import *
 name = ""
 password = ""
+fl = open("/home/suraj/Documents/personal/ExoCrypot/credentials.txt")
+cred = fl.read().split()
+user = cred[0]
+pasw = cred[1]
 
 def submit():
     global name, password
     name = name_var.get()
     password = passw_var.get()
+    credentials(name, password)
+
+def credentials(guser, gpass):
+    global user, pasw
+    if user == guser and gpass == pasw:
+        msg.insert(END, "Success!")
+    else:
+        msg.insert(END, "Recheck username and password!")
 
 root = Tk()
 root.title("ExoCrypt-Login")
@@ -31,6 +43,8 @@ passw_entry = Entry(root, show ="*", textvariable=passw_var)
 passw_disp = Label(root, text="Password", bg='#322C2C', fg="#ffffff")
 passw_disp.place(x=366, y=340)
 passw_entry.place(x=445, y=338)
+msg = Text(root, height=1, width=50,bg="#322C2C",fg="#ffffff")
+msg.place(x=300, y=420)
 sub_btn = Button(root, text="Submit", command=submit)
 sub_btn.place(x=460, y=380)
 back_button = Button(root, text="Back", command=root.destroy)
