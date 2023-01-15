793398ae8d2cf1f268702fa56f7cd558356d9bc0
surajkadapa <surajkadapa103@gmail.com>
2023-01-15 20:01:42 +0530

    Added sending email from the main program.

--- app.py
+++ app.py
@@ -1,25 +1,86 @@
 import tkinter as tk
 from tkinter import * 
 import base64
+import email, smtplib, ssl
+sender_email = "testpython333@gmail.com"
+passwrd = "wkpltoxkaklffbpm"
+send_addr = ""
+body = ""
 m = Tk()
+m3 = None
 #322c2c
-
+login_pass = False
 m.title('ExoCrypt')
 m.geometry('950x700')
 m['bg']='#322c2c'
 m.resizable(False,False)
 name = ""
 password = ""
-fl = open("credentials.txt") #the path problem should be taken care of
+fl = open("/home/suraj/Documents/personal/ExoCrypot/credentials.txt") #the path problem should be taken care of
 cred = fl.read().split()
 user = cred[0]
 pasw = cred[1]
 
+def send_main():
+    global send_addr
+    global passwrd
+    global body
+    global to_addr
+    global m3
+    def get_deets():
+        global send_addr
+        global body
+        send_addr = to_addr.get("1.0", "end-1c")
+        body = body.get("1.0", "end-1c")
+    
+    def send_email():
+        get_deets()
+        context = ssl.create_default_context()
+        print(sender_email, passwrd)
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, passwrd)
+            server.sendmail(sender_email, send_addr, body)
+
+
+
+    m3 = Tk()
+    m3.title("ExoCrypt-Send Email")
+    m3.geometry('950x700')
+    m3.resizable(False, False)
+    m3 ['bg'] = "#322C2C"
+    back_button = Button(m3, text="Back", command=m3.destroy)
+    back_button.place(x=0, y=0)
+    comic =("Comic Sans MS",32,"bold")
+    head=Label(m3, bg="#322c2c", fg="#ffffff", text="ExoCrypt", font=comic)
+    head.configure(justify="center")
+    head.place(x=395, y=20)
+    comic1 = ("Ubuntu", 15, "bold")
+    title = Label(m3, bg="#322c2c", fg="#ffffff", text="Send Email", font=comic1)
+    title.configure(justify="center")
+    title.place(x=420, y=76)
+    to_card = Label(m3, text="To:", font=21)
+    to_card.place(x=90,y=150)
+    to_addr = Text(m3, height=1, width=50)
+    to_addr.place(x=160,y=149)
+    attachment = Button(m3, text="Attach Files") #needs to be worked on
+    attachment.place(x=650, y=154)
+    body_card = Label(m3, text="Body: ", font=21)
+    body_card.place(x=90, y=193)
+    body = Text(m3, height=20, width=80)
+    body.place(x=160, y=190)
+    submit = Button(m3, text="Submit", command=send_email)
+    submit.place(x=685, y=550)
+    m3.mainloop()
+
+
+
 #login page
 
 def login():
+    global login_pass
     global m
     global user, pasw
+    global m3
     print(user,pasw)
     m.destroy()
     def my_show():
@@ -38,12 +99,15 @@ def login():
         credentials(name, password)
 
     def credentials(guser, gpass):
-        global user, pasw
+        global user, pasw, login_pass
         #print(guser, gpass)
         if user == guser and gpass == pasw:
             msg.insert(END, "Success!")
+            root.destroy()
+            send_main()
         else:
             msg.insert(END, "Recheck username and password!")
+    
 
     root = tk.Tk()
     root.title("ExoCrypt-Login")
@@ -117,6 +181,10 @@ def decry():
     dec_button.place(x=410,y=550)
     m2.mainloop()
 
+
+#Send email page
+
+
 #Exocrypt heading
 comic=("Comic Sans MS",32,"bold")
 head=tk.Label(m,bg='#322c2c',fg='#ffffff',text='ExoCrypt',font=comic)
@@ -124,7 +192,7 @@ head.place(x=395, y=175)
 head.configure(justify='center')
 
 #logo
-logo = PhotoImage(file="exo_1.png")
+logo = PhotoImage(file="/home/suraj/Documents/personal/ExoCrypot/exo_1.png")
 Label(image=logo,bg="#322C2C").place(x=385, y=230)
 
 #Send Button
@@ -137,5 +205,6 @@ decry.place(x=505,y=430)
 
 credits = tk.Label(m, bg="#322C2C", fg="#ffffff", text="By").place(x=840, y=638)
 credits2 = tk.Label(m, bg="#322C2C", fg="#ffffff", text="Suraj, Justin, Ishaan").place(x=790, y=660)
+
 
 m.mainloop()
